96a4b4153c63dc1cc665b57654c324806776cb5e
VendanAndrews <VendanAndrews99@gmail.com>
2013-03-26 17:31:55 -0400

    Initial Commit

---
+++ MinerBot/MinerBot.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MinerBot
+{
+    partial class MinerBot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblState = new System.Windows.Forms.Label();
+            this.txtStation = new System.Windows.Forms.TextBox();
+            this.lblStation = new System.Windows.Forms.Label();
+            this.chkActive = new System.Windows.Forms.CheckBox();
+            this.btnCurrentStation = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblCurRoidOre = new System.Windows.Forms.Label();
+            this.lblEstimatedMined = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(12, 9);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(38, 13);
+            this.lblState.TabIndex = 0;
+            this.lblState.Text = "State: ";
+            // 
+            // txtStation
+            // 
+            this.txtStation.Location = new System.Drawing.Point(58, 59);
+            this.txtStation.Name = "txtStation";
+            this.txtStation.Size = new System.Drawing.Size(100, 20);
+            this.txtStation.TabIndex = 1;
+            this.txtStation.TextChanged += new System.EventHandler(this.txtStation_TextChanged);
+            // 
+            // lblStation
+            // 
+            this.lblStation.AutoSize = true;
+            this.lblStation.Location = new System.Drawing.Point(12, 62);
+            this.lblStation.Name = "lblStation";
+            this.lblStation.Size = new System.Drawing.Size(40, 13);
+            this.lblStation.TabIndex = 2;
+            this.lblStation.Text = "Station";
+            // 
+            // chkActive
+            // 
+            this.chkActive.AutoSize = true;
+            this.chkActive.Location = new System.Drawing.Point(15, 85);
+            this.chkActive.Name = "chkActive";
+            this.chkActive.Size = new System.Drawing.Size(56, 17);
+            this.chkActive.TabIndex = 3;
+            this.chkActive.Text = "Active";
+            this.chkActive.UseVisualStyleBackColor = true;
+            this.chkActive.CheckedChanged += new System.EventHandler(this.chkActive_CheckedChanged);
+            // 
+            // btnCurrentStation
+            // 
+            this.btnCurrentStation.Location = new System.Drawing.Point(164, 57);
+            this.btnCurrentStation.Name = "btnCurrentStation";
+            this.btnCurrentStation.Size = new System.Drawing.Size(90, 23);
+            this.btnCurrentStation.TabIndex = 4;
+            this.btnCurrentStation.Text = "Current Station";
+            this.btnCurrentStation.UseVisualStyleBackColor = true;
+            this.btnCurrentStation.Click += new System.EventHandler(this.btnCurrentStation_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblCurRoidOre
+            // 
+            this.lblCurRoidOre.AutoSize = true;
+            this.lblCurRoidOre.Location = new System.Drawing.Point(12, 149);
+            this.lblCurRoidOre.Name = "lblCurRoidOre";
+            this.lblCurRoidOre.Size = new System.Drawing.Size(71, 13);
+            this.lblCurRoidOre.TabIndex = 5;
+            this.lblCurRoidOre.Text = "CurRoid Ore: ";
+            // 
+            // lblEstimatedMined
+            // 
+            this.lblEstimatedMined.AutoSize = true;
+            this.lblEstimatedMined.Location = new System.Drawing.Point(12, 173);
+            this.lblEstimatedMined.Name = "lblEstimatedMined";
+            this.lblEstimatedMined.Size = new System.Drawing.Size(91, 13);
+            this.lblEstimatedMined.TabIndex = 6;
+            this.lblEstimatedMined.Text = "Estimated Mined: ";
+            // 
+            // MinerBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 206);
+            this.Controls.Add(this.lblEstimatedMined);
+            this.Controls.Add(this.lblCurRoidOre);
+            this.Controls.Add(this.btnCurrentStation);
+            this.Controls.Add(this.chkActive);
+            this.Controls.Add(this.lblStation);
+            this.Controls.Add(this.txtStation);
+            this.Controls.Add(this.lblState);
+            this.Name = "MinerBot";
+            this.Text = "MinerBot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.TextBox txtStation;
+        private System.Windows.Forms.Label lblStation;
+        private System.Windows.Forms.CheckBox chkActive;
+        private System.Windows.Forms.Button btnCurrentStation;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCurRoidOre;
+        private System.Windows.Forms.Label lblEstimatedMined;
+
+    }
+}
+
---
+++ MinerBot/MinerBot.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using EveCom;
+
+namespace MinerBot
+{
+    public partial class MinerBot : Form
+    {
+        public MinerBot()
+        {
+            InitializeComponent();
+        }
+
+        enum State
+        {
+            InStation,
+            Unloaded,
+            HeadingToBelt,
+            InBelt,
+            HeadingToStation,
+            Defensive,
+        }
+
+        State CurState;
+        DateTime NextPulse;
+        Queue<Entity> Belts = new Queue<Entity>();
+        Entity CurBelt = null;
+        Entity CurRoid = null;
+        Entity CurTarget = null;
+        double CurRoidOre = 0;
+        double EstimatedMined = 0;
+        bool Active = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (new EVEFrameLock())
+            {
+            }
+            CurState = State.InStation;
+            NextPulse = DateTime.Now.AddSeconds(1);
+            EVEFrame.OnFrame += new EventHandler(EVEFrame_OnFrame);
+            txtStation.Text = Properties.Settings.Default.Station;
+        }
+
+        void EVEFrame_OnFrame(object sender, EventArgs e)
+        {
+            if (!Active)
+            {
+                return;
+            }
+            if (Session.Safe && Session.NextSessionChange < Session.Now)
+            {
+                //lblState.Text = "State: " + CurState.ToString();
+                if (DateTime.Now > NextPulse)
+                {
+                    NextPulse = DateTime.Now.AddSeconds(2);
+                    switch (CurState)
+                    {
+                        case State.InStation:
+                            if (Session.InSpace)
+                            {
+                                CurState = State.InBelt;
+                                return;
+                            }
+                            if (MyShip.OreHold == null)
+                            {
+                                EVEFrame.Log("Opening Inventory");
+                                Command.OpenInventory.Execute();
+                                return;
+                            }
+                            if (!MyShip.OreHold.IsPrimed)
+                            {
+                                EVEFrame.Log("Making OreHold Active");
+                                MyShip.OreHold.MakeActive();
+                            }
+                            if (MyShip.OreHold.Items.Count(item => item.CategoryID == Category.Asteroid) > 0)
+                            {
+                                EVEFrame.Log("Transfering Ore");
+                                MyShip.OreHold.Items.Where(item => item.CategoryID == Category.Asteroid).MoveTo(Station.ItemHangar);
+                            }
+                            CurState = State.Unloaded;
+                            break;
+                        case State.Unloaded:
+                            if (Session.InStation)
+                            {
+                                EVEFrame.Log("Undocking");
+                                Command.CmdExitStation.Execute();
+                                NextPulse = DateTime.Now.AddSeconds(10);
+                                return;
+                            }
+                            CurState = State.InBelt;
+                            break;
+                        case State.InBelt:
+                            if (Entity.All.Count(ent => ent.IsTargetingMe) > 0)
+                            {
+                                CurState = State.Defensive;
+                                return;
+                            }
+
+                            if (Entity.All.Count(ent => ent.CategoryID == Category.Asteroid) == 0)
+                            {
+                                CurBelt = null;
+                                CurState = State.HeadingToBelt;
+                                return;
+                            }
+                            if (MyShip.OreHold == null)
+                            {
+                                EVEFrame.Log("Opening Inventory");
+                                Command.OpenInventory.Execute();
+                                return;
+                            }
+                            if (!MyShip.OreHold.IsPrimed)
+                            {
+                                EVEFrame.Log("Making OreHold Active");
+                                MyShip.OreHold.MakeActive();
+                            }
+                            if (MyShip.OreHold.UsedCapacity > MyShip.OreHold.MaxCapacity * 0.75)
+                            {
+                                EVEFrame.Log("Returning To Station");
+                                CurState = State.HeadingToStation;
+                                return;
+                            }
+                            if (CurRoid == null || !CurRoid.Exists)
+                            {
+                                CurRoid = Entity.All.Where(ent => ent.CategoryID == Category.Asteroid).OrderBy(ent => ent.Distance).First();
+                            }
+                            Func<Module, bool> MiningLasers = (mod) => mod.GroupID == Group.FrequencyMiningLaser || mod.GroupID == Group.MiningLaser || mod.GroupID == Group.StripMiner;
+
+                            if (CurRoid.Distance > MyShip.Modules.Where(MiningLasers).Min(mod => mod.OptimalRange))
+                            {
+                                if(MyShip.ToEntity.Mode == EntityMode.Approaching)
+                                {
+                                    return; 
+                                }
+                                EVEFrame.Log("Approaching " + CurRoid.Name);
+                                CurRoid.Approach((int)(MyShip.Modules.Where(MiningLasers).Min(mod => mod.OptimalRange) * 0.75));
+                                return;
+                            }
+
+                            if (!CurRoid.LockedTarget && !CurRoid.LockingTarget)
+                            {
+                                EVEFrame.Log("Locking " + CurRoid.Name);
+                                CurRoid.LockTarget();
+                                return;
+                            }
+
+                            if(MyShip.Modules.Count(mod => mod.GroupID == Group.SurveyScanner) > 0)
+                            {
+                                if (!SurveyScan.Scan.ContainsKey(CurRoid) && !MyShip.Modules.First(mod => mod.GroupID == Group.SurveyScanner).IsActive)
+                                {
+                                    EVEFrame.Log("Activating Scanner");
+                                    MyShip.Modules.First(mod => mod.GroupID == Group.SurveyScanner).Activate();
+                                    return;
+                                }
+                                if (SurveyScan.Scan.ContainsKey(CurRoid))
+                                {
+                                    CurRoidOre = SurveyScan.Scan[CurRoid] * CurRoid.Volume;
+                                    EstimatedMined = (double)MyShip.Modules.Where(MiningLasers).Sum(mod => mod.MiningYield * mod.TotalCompletion);
+
+                                    if (MyShip.Modules.Where(MiningLasers).Sum(mod => mod.MiningYield * mod.TotalCompletion) > (SurveyScan.Scan[CurRoid] * CurRoid.Volume + 5))
+                                    {
+                                        EVEFrame.Log("ShortCycling Lasers");
+                                        foreach (Module laser in MyShip.Modules.Where(MiningLasers))
+                                        {
+                                            laser.Deactivate();
+                                        }
+                                    }
+                                }
+                            }
+
+                            if (MyShip.Modules.Where(MiningLasers).Count(mod => !mod.IsActive) > 0 && CurRoid.LockedTarget)
+                            {
+                                EVEFrame.Log("Mining " + CurRoid.Name);
+                                MyShip.Modules.Where(MiningLasers).First(mod => !mod.IsActive).Activate(CurRoid);
+                            }
+                            break;
+                        case State.HeadingToBelt:
+                            if (CurBelt == null || !CurBelt.Exists)
+                            {
+                                if (Belts.Count == 0)
+                                {
+                                    Belts = new Queue<Entity>(Entity.All.Where(ent => ent.GroupID == Group.AsteroidBelt));
+                                }
+                                CurBelt = Belts.Dequeue();
+                                return;
+                            }
+                            if (MyShip.ToEntity.Mode != EntityMode.Warping && CurBelt.Distance > 100000)
+                            {
+                                EVEFrame.Log("Warping To " + CurBelt.Name);
+                                CurBelt.WarpTo();
+                                NextPulse = DateTime.Now.AddSeconds(10);
+                                return;
+                            }
+                            if (CurBelt.Distance < 100000)
+                            {
+                                CurState = State.InBelt;
+                            }
+                            break;
+                        case State.HeadingToStation:
+                            if (Session.InStation)
+                            {
+                                CurState = State.InStation;
+                                return;
+                            }
+                            if (MyShip.ToEntity.Mode != EntityMode.Warping)
+                            {
+                                Entity station = Entity.All.FirstOrDefault(ent => ent.GroupID == Group.Station && ent.Name == Properties.Settings.Default.Station);
+                                if (station != null && station.Exists)
+                                {
+                                    EVEFrame.Log("Docking At " + CurRoid.Name);
+                                    station.Dock();
+                                    NextPulse = DateTime.Now.AddSeconds(10);
+                                    return;
+                                }
+                            }
+                            break;
+                        case State.Defensive:
+                            if (Entity.All.Count(ent => ent.IsTargetingMe) == 0)
+                            {
+                                Drone.AllInSpace.Where(drone => drone.State != EntityState.Departing && drone.State != EntityState.Departing_2).ReturnToDroneBay();
+                                if (Drone.AllInSpace.Count() > 0)
+                                {
+                                    return;
+                                }
+                                CurState = State.InBelt;
+                                return;
+                            }
+                            if (CurTarget == null || CurTarget.Exploded)
+                            {
+                                CurTarget = Entity.All.First(ent => ent.IsTargetingMe);
+                                return;
+                            }
+                            if (CurTarget.Distance < MyShip.MaxTargetRange)
+                            {
+                                CurTarget.LockTarget();
+                            }
+                            if (!CurTarget.IsActiveTarget)
+                            {
+                                CurTarget.MakeActive();
+                            }
+                            if (CurTarget.Distance < 20000 && CurTarget.IsActiveTarget && Drone.AllInSpace.Count(drone => drone.GroupID == Group.CombatDrone && drone.Target != CurTarget) > 0)
+                            {
+                                Drone.AllInSpace.Where(drone => drone.GroupID == Group.CombatDrone && drone.Target != CurTarget).Attack();
+                                NextPulse = DateTime.Now.AddSeconds(5);
+                                return;
+                            }
+                            if (Drone.AllInBay.Count(drone => drone.GroupID == Group.CombatDrone) > 0 && Me.MaxActiveDrones > Drone.AllInSpace.Count())
+                            {
+                                Drone.AllInBay.Where(drone => drone.GroupID == Group.CombatDrone).Take(Me.MaxActiveDrones - Drone.AllInSpace.Count()).Launch();
+                                NextPulse = DateTime.Now.AddSeconds(5);
+                                return;
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void txtStation_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Station = txtStation.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void chkActive_CheckedChanged(object sender, EventArgs e)
+        {
+            Active = chkActive.Checked;
+        }
+
+        private void btnCurrentStation_Click(object sender, EventArgs e)
+        {
+            using(new EVEFrameLock())
+            {
+                txtStation.Text = Session.StationName;
+                Properties.Settings.Default.Station = txtStation.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblState.Text = "State: " + CurState.ToString();
+            lblCurRoidOre.Text = "CurRoid Ore: " + CurRoidOre.ToString("F2") + " m^3";
+            lblEstimatedMined.Text = "EstimatedMined: " + EstimatedMined.ToString("F2") + " m^3";
+        }
+    }
+}
